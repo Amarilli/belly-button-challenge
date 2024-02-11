252220aa2048111c4ad3c61b289cd51e2919fe65
Ama <amarillinovel@Ami.local>
2024-02-11 12:17:43 -0600

    removing cons URL on bonus

--- static/js/bonus.js
+++ static/js/bonus.js
@@ -1,6 +1,3 @@
-// Create a constant variable where to place the URL
-const URL = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json'
-
 // Fetch the json data and console log it
 d3.json(URL).then(function(data) {
     console.log(data);
